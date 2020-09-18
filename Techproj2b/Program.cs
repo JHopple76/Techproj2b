565425e3e9033d243953e8273522d197ab1221f1
Thomas Hopple <hopple@usf.edu>
2020-09-17 21:45:26 -0400

    Add project files.

---
+++ Techproj2b/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Techproj2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
